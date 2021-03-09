ef49fe457e393a958a8ab754567555092722dd64
FadingShadow <warsquad0@gmail.com>
2021-03-10 02:54:39 +0500

    Ok done

---
+++ config/laratrust_seeder.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    /**
+     * Control if the seeder should create a user per role while seeding the data.
+     */
+    'create_users' => true,
+
+    /**
+     * Control if all the laratrust tables should be truncated before running the seeder.
+     */
+    'truncate_tables' => true,
+
+    'roles_structure' => [
+        'department_head' => [
+            'users' => 'c,r,u,d',
+        ],
+        'buyer' => [
+            'profile' => 'r,u',
+        ],
+    ],
+
+    'permissions_map' => [
+        'c' => 'create',
+        'r' => 'read',
+        'u' => 'update',
+        'd' => 'delete',
+        'm_e' => 'manage-expenses',
+    ]
+];
